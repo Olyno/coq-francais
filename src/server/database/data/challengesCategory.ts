99c61d617e33250fe4e00b267a3f12f4500066ae
Noemie21 <57950292+Noemie21@users.noreply.github.com>
2020-02-07 12:41:09 +0100

    Update challengesCategory.ts

--- src/server/database/data/challengesCategory.ts
+++ src/server/database/data/challengesCategory.ts
@@ -8,14 +8,70 @@ export default [
                 name: 'Regular Walker',
                 description: 'Walk 20 minutes a day for 1 week',
                 icon: '/icons/challenges/challenge1.svg'
-            }
+            },
+            {   
+                name: 'Advanced Walker',
+                description: 'Walk 40 minutes a day for 1 week',
+                icon: '/icons/challenges/challenge1.svg'
+            },
+            {   
+                name: 'Experimented Walker',
+                description: 'Walk 1 hour a day for 1 week',
+                icon: '/icons/challenges/challenge1.svg'
+            },
+            {   
+                name: 'Short distance Walker',
+                description: 'Walk 6km per week',
+                icon: '/icons/challenges/challenge1.svg'
+            },
+            {   
+                name: 'Medium distance Walker',
+                description: 'Walk 10km per week',
+                icon: '/icons/challenges/challenge1.svg'
+            },
+            {   
+                name: 'Long distances Walker',
+                description: 'Walk 15km per week',
+                icon: '/icons/challenges/challenge1.svg'
+            },
         ]
     },
     {
         name: 'To Start',
         icon: '/icons/challenges/challenge2.svg',
         color: '#D7FFCD',
-        challenges: []
+        challenges: [
+            {
+                name: 'Run one week',
+                description: 'Run 2km per day for 1 week',
+                icon: '/icons/challenges/pourcommencer.svg',
+            },
+            {
+                name: 'Run two weeks',
+                description: 'Run 2km per day for 2 weeks',
+                icon: '/icons/challenges/pourcommencer.svg',
+            },
+            {
+                name: 'Run a month',
+                description: 'Run 2km per day for 1 month',
+                icon: '/icons/challenges/pourcommencer.svg',
+            },
+            {
+                name: 'Bike one week',
+                description: 'Ride a bike 1 hour a day for 1 week',
+                icon: '/icons/challenges/pourcommencer2.svg',
+            },
+            {
+                name: 'Bike two weeks',
+                description: 'Ride a bike 1 hour a day for 2 weeks',
+                icon: '/icons/challenges/pourcommencer2.svg',
+            },
+            {
+                name: 'Bike a month',
+                description: 'Ride a bike 1 hour a day for 1 month',
+                icon: '/icons/challenges/pourcommencer2.svg',
+            },
+        ]
     },
     {
         name: 'Extremes sports',
@@ -27,7 +83,99 @@ export default [
         name: 'Normal sports',
         icon: '/icons/challenges/challenge4.svg',
         color: '#D79AFD',
-        challenges: []
+        challenges: [
+            {
+                name: 'Tennis beginner',
+                description: 'Play 1 match against your friends',
+                icon: '/icons/challenges/tennis.svg',
+            },
+            {
+                name: 'Tennis advanced',
+                description: 'Play 1 per month against your friends',
+                icon: '/icons/challenges/tennis.svg',
+            },
+            {
+                name: 'Tennis experimented',
+                description: 'Play 1 match per week for 2 months',
+                icon: '/icons/challenges/tennis.svg',
+            },
+            {
+                name: 'Footy beginner',
+                description: 'Play 1 match with your friends',
+                icon: '/icons/challenges/footy.svg',
+            },
+            {
+                name: 'Footy advanced',
+                description: 'Play 1 per month with your friends',
+                icon: '/icons/challenges/footy.svg',
+            },
+            {
+                name: 'Footy experimented',
+                description: 'Play 1 match per week for 2 months',
+                icon: '/icons/challenges/footy.svg',
+            },
+            {
+                name: 'Natation beginner',
+                description: 'Go to the pool 5 times per month - alone or with your friends',
+                icon: '/icons/challenges/natation.svg',
+            },
+            {
+                name: 'Natation advanced',
+                description: 'Go swimming 10 times in a month',
+                icon: '/icons/challenges/natation.svg',
+            },
+            {
+                name: 'Natation experimented',
+                description: 'Go to the pool 1 time per week for 2 months ',
+                icon: '/icons/challenges/natation.svg',
+            },
+            {
+                name: 'Basketball beginner',
+                description: 'Play 1 match with your friends',
+                icon: '/icons/challenges/basket.svg',
+            },
+            {
+                name: 'Basketball advanced',
+                description: 'Play 1 per month with your friends',
+                icon: '/icons/challenges/basket.svg',
+            },
+            {
+                name: 'Basketball experimented',
+                description: 'Play 1 match per week for 2 months',
+                icon: '/icons/challenges/basket.svg',
+            },
+            {
+                name: 'Football beginner',
+                description: 'Play 1 match with your friends',
+                icon: '/icons/challenges/football.svg',
+            },
+            {
+                name: 'Football advanced',
+                description: 'Play 1 per month with your friends',
+                icon: '/icons/challenges/football.svg',
+            },
+            {
+                name: 'Football experimented',
+                description: 'Play 1 match per week for 2 months',
+                icon: '/icons/challenges/football.svg',
+            },
+            {
+                name: 'Rugby beginner',
+                description: 'Play 1 match with your friends',
+                icon: '/icons/challenges/rugby.svg',
+            },
+            {
+                name: 'Rugby advanced',
+                description: 'Play 1 per month with your friends',
+                icon: '/icons/challenges/rugby.svg',
+            },
+            {
+                name: 'Rugby experimented',
+                description: 'Play 1 match per week for 2 months',
+                icon: '/icons/challenges/rugby.svg',
+            },
+            
+        ]
     },
     {
         name: 'Handisport',
