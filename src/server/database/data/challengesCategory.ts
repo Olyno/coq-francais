651cecf7d7f9150c17faee412c2708089ac72b03
Noemie21 <57950292+Noemie21@users.noreply.github.com>
2020-02-07 14:31:59 +0100

    Update challengesCategory.ts

--- src/server/database/data/challengesCategory.ts
+++ src/server/database/data/challengesCategory.ts
@@ -7,32 +7,38 @@ export default [
             {
                 name: 'Regular Walker',
                 description: 'Walk 20 minutes a day for 1 week',
-                icon: '/icons/challenges/challenge1.svg'
+                icon: '/icons/challenges/challenge1.svg',
+                points:'100',
             },
             {   
                 name: 'Advanced Walker',
                 description: 'Walk 40 minutes a day for 1 week',
-                icon: '/icons/challenges/challenge1.svg'
+                icon: '/icons/challenges/challenge1.svg',
+                points:'150',
             },
             {   
                 name: 'Experimented Walker',
                 description: 'Walk 1 hour a day for 1 week',
-                icon: '/icons/challenges/challenge1.svg'
+                icon: '/icons/challenges/challenge1.svg',
+                points:'200',
             },
             {   
                 name: 'Short distance Walker',
                 description: 'Walk 6km per week',
-                icon: '/icons/challenges/challenge1.svg'
+                icon: '/icons/challenges/challenge1.svg',
+                points:'100',
             },
             {   
                 name: 'Medium distance Walker',
                 description: 'Walk 10km per week',
-                icon: '/icons/challenges/challenge1.svg'
+                icon: '/icons/challenges/challenge1.svg',
+                points:'150',
             },
             {   
                 name: 'Long distances Walker',
                 description: 'Walk 15km per week',
-                icon: '/icons/challenges/challenge1.svg'
+                icon: '/icons/challenges/challenge1.svg',
+                points:'200',
             },
         ]
     },
@@ -45,31 +51,37 @@ export default [
                 name: 'Run one week',
                 description: 'Run 2km per day for 1 week',
                 icon: '/icons/challenges/challenge2.svg',
+                points:'200',
             },
             {
                 name: 'Run two weeks',
                 description: 'Run 2km per day for 2 weeks',
                 icon: '/icons/challenges/challenge2.svg',
+                points:'300',
             },
             {
                 name: 'Run a month',
                 description: 'Run 2km per day for 1 month',
                 icon: '/icons/challenges/challenge2.svg',
+                points:'400',
             },
             {
                 name: 'Bike one week',
                 description: 'Ride a bike 1 hour a day for 1 week',
                 icon: '/icons/challenges/tostart2.svg',
+                points:'150',
             },
             {
                 name: 'Bike two weeks',
                 description: 'Ride a bike 1 hour a day for 2 weeks',
                 icon: '/icons/challenges/tostart2.svg',
+                points:'250',
             },
             {
                 name: 'Bike a month',
                 description: 'Ride a bike 1 hour a day for 1 month',
                 icon: '/icons/challenges/tostart2.svg',
+                points:'350',
             },
         ]
     },
@@ -77,7 +89,86 @@ export default [
         name: 'Extremes sports',
         icon: '/icons/challenges/challenge3.svg',
         color: '#CDFFF7',
-        challenges: []
+        challenges: [
+            {
+                name: 'Rafting beginner',
+                description: 'Raft 4 times in a month',
+                icon: '/icons/challenges/rafting.svg',
+                points:'150',
+            },
+            {
+                name: 'Rafting advanced',
+                description: 'Raft 6km',
+                icon: '/icons/challenges/rafting.svg',
+                points:'200',
+            },
+            {
+                name: 'Rafting experimented',
+                description: 'Raft 1 time per week for 2 months ',
+                icon: '/icons/challenges/rafting.svg',
+                points:'300',
+            },
+            {
+                name: 'Rafting master',
+                description: 'Raft 12km',
+                icon: '/icons/challenges/rafting.svg',
+                points:'400',
+            },
+            {
+                name: 'Climbing beginner',
+                description: 'Climb 4 times in a month',
+                icon: '/icons/challenges/escalade.svg',
+                points:'150',
+            },
+            {
+                name: 'Climbing advanced',
+                description: 'Climb 10 times in a month',
+                icon: '/icons/challenges/escalade.svg',
+                points:'300',
+            },
+            {
+                name: 'Climbing experimented',
+                description: 'Climb 1 time per week for 2 months',
+                icon: '/icons/challenges/escalade.svg',
+                points:'400',
+            },
+            {
+                name: 'Surf beginner',
+                description: 'Surf 4 times in a month',
+                icon: '/icons/challenges/surf.svg',
+                points:'150',
+            },
+            {
+                name: 'Surf advanced',
+                description: 'Surf 10 times in a month',
+                icon: '/icons/challenges/surf.svg',
+                points:'300',
+            },
+            {
+                name: 'Surf experimented',
+                description: 'Surf 1 time per week for 2 months',
+                icon: '/icons/challenges/surf.svg',
+                points:'400',
+            },
+            {
+                name: 'Sail beginner',
+                description: 'Sail 4 times in a month',
+                icon: '/icons/challenges/voile.svg',
+                points:'150',
+            },
+            {
+                name: 'Sail advanced',
+                description: 'Sail 10 times in a month',
+                icon: '/icons/challenges/voile.svg',
+                points:'300',
+            },
+            {
+                name: 'Sail experimented',
+                description: 'Sail 1 time per week for 2 months',
+                icon: '/icons/challenges/voile.svg',
+                points:'400',
+            },
+        ]
     },
     {
         name: 'Normal sports',
@@ -88,91 +179,109 @@ export default [
                 name: 'Tennis beginner',
                 description: 'Play 1 match against your friends',
                 icon: '/icons/challenges/tennis.svg',
+                points:'50',
             },
             {
                 name: 'Tennis advanced',
                 description: 'Play 1 per month against your friends',
                 icon: '/icons/challenges/tennis.svg',
+                points:'150',
             },
             {
                 name: 'Tennis experimented',
                 description: 'Play 1 match per week for 2 months',
                 icon: '/icons/challenges/tennis.svg',
+                points:'400',
             },
             {
                 name: 'Footy beginner',
                 description: 'Play 1 match with your friends',
                 icon: '/icons/challenges/footy.svg',
+                points:'50',
             },
             {
                 name: 'Footy advanced',
                 description: 'Play 1 per month with your friends',
                 icon: '/icons/challenges/footy.svg',
+                points:'150',
             },
             {
                 name: 'Footy experimented',
                 description: 'Play 1 match per week for 2 months',
                 icon: '/icons/challenges/footy.svg',
+                points:'400',
             },
             {
                 name: 'Natation beginner',
                 description: 'Go to the pool 5 times per month - alone or with your friends',
                 icon: '/icons/challenges/natation.svg',
+                points:'50',
             },
             {
                 name: 'Natation advanced',
                 description: 'Go swimming 10 times in a month',
                 icon: '/icons/challenges/natation.svg',
+                points:'150',
             },
             {
                 name: 'Natation experimented',
                 description: 'Go to the pool 1 time per week for 2 months ',
                 icon: '/icons/challenges/natation.svg',
+                points:'300',
             },
             {
                 name: 'Basketball beginner',
                 description: 'Play 1 match with your friends',
                 icon: '/icons/challenges/basket.svg',
+                points:'50',
             },
             {
                 name: 'Basketball advanced',
                 description: 'Play 1 per month with your friends',
                 icon: '/icons/challenges/basket.svg',
+                points:'150',
             },
             {
                 name: 'Basketball experimented',
                 description: 'Play 1 match per week for 2 months',
                 icon: '/icons/challenges/basket.svg',
+                points:'400',
             },
             {
                 name: 'Football beginner',
                 description: 'Play 1 match with your friends',
                 icon: '/icons/challenges/football.svg',
+                points:'50',
             },
             {
                 name: 'Football advanced',
                 description: 'Play 1 per month with your friends',
                 icon: '/icons/challenges/football.svg',
+                points:'150',
             },
             {
                 name: 'Football experimented',
                 description: 'Play 1 match per week for 2 months',
                 icon: '/icons/challenges/football.svg',
+                points:'400',
             },
             {
                 name: 'Rugby beginner',
                 description: 'Play 1 match with your friends',
                 icon: '/icons/challenges/rugby.svg',
+                points:'50',
             },
             {
                 name: 'Rugby advanced',
                 description: 'Play 1 per month with your friends',
                 icon: '/icons/challenges/rugby.svg',
+                points:'150',
             },
             {
                 name: 'Rugby experimented',
                 description: 'Play 1 match per week for 2 months',
                 icon: '/icons/challenges/rugby.svg',
+                points:'400',
             },
             
         ]
@@ -181,12 +290,144 @@ export default [
         name: 'Handisport',
         icon: '/icons/challenges/challenge5.svg',
         color: '#FAE2C2',
-        challenges: []
+        challenges: [
+            {
+                name: 'Regular Walker',
+                description: 'Walk 20 minutes a day for 1 week',
+                icon: '/icons/challenges/challenge1.svg',
+                points:'100',
+            },
+            {   
+                name: 'Advanced Walker',
+                description: 'Walk 40 minutes a day for 1 week',
+                icon: '/icons/challenges/challenge1.svg',
+                points:'150',
+            },
+            {   
+                name: 'Experimented Walker',
+                description: 'Walk 1 hour a day for 1 week',
+                icon: '/icons/challenges/challenge1.svg',
+                points:'200',
+            },
+            {   
+                name: 'Short distance Walker',
+                description: 'Walk 6km per week',
+                icon: '/icons/challenges/challenge1.svg',
+                points:'100',
+            },
+            {   
+                name: 'Medium distance Walker',
+                description: 'Walk 10km per week',
+                icon: '/icons/challenges/challenge1.svg',
+                points:'150',
+            },
+            {   
+                name: 'Long distances Walker',
+                description: 'Walk 15km per week',
+                icon: '/icons/challenges/challenge1.svg',
+                points:'200',
+            },
+            {
+                name: 'Run one week',
+                description: 'Run 2km per day for 1 week',
+                icon: '/icons/challenges/challenge2.svg',
+                points:'200',
+            },
+            {
+                name: 'Run two weeks',
+                description: 'Run 2km per day for 2 weeks',
+                icon: '/icons/challenges/challenge2.svg',
+                points:'300',
+            },
+            {
+                name: 'Run a month',
+                description: 'Run 2km per day for 1 month',
+                icon: '/icons/challenges/challenge2.svg',
+                points:'400',
+            },
+            {
+                name: 'Bike one week',
+                description: 'Ride a bike 1 hour a day for 1 week',
+                icon: '/icons/challenges/tostart2.svg',
+                points:'150',
+            },
+            {
+                name: 'Bike two weeks',
+                description: 'Ride a bike 1 hour a day for 2 weeks',
+                icon: '/icons/challenges/tostart2.svg',
+                points:'250',
+            },
+            {
+                name: 'Bike a month',
+                description: 'Ride a bike 1 hour a day for 1 month',
+                icon: '/icons/challenges/tostart2.svg',
+                points:'350',
+            },
+            {
+                name: 'Natation beginner',
+                description: 'Go to the pool 5 times per month - alone or with your friends',
+                icon: '/icons/challenges/natation.svg',
+                points:'50',
+            },
+            {
+                name: 'Natation advanced',
+                description: 'Go swimming 10 times in a month',
+                icon: '/icons/challenges/natation.svg',
+                points:'150',
+            },
+            {
+                name: 'Natation experimented',
+                description: 'Go to the pool 1 time per week for 2 months ',
+                icon: '/icons/challenges/natation.svg',
+                points:'300',
+            },
+            {
+                name: 'Information1',
+                description: 'There are infrastructures where you can practice the sport you want in club.',
+                icon: '/icons/challenges/informations.svg',
+            },
+            {
+                name: 'Information2',
+                description: 'Even if your mobility is reduced, you can practice the sport you like.',
+                icon: '/icons/challenges/informations.svg',
+            },
+        ]
     },
     {
         name: 'Healthy',
         icon: '/icons/challenges/challenge6.svg',
         color: '#F5F9AA',
-        challenges: []
+        challenges: [
+            {
+                name: 'Phone',
+                description: 'Use your phone only 2 p.m. for a week',
+                icon: '',
+                points:'150',
+            },
+            {
+                name: 'Eat',
+                description: 'Eat a dish with seasonal vegetables',
+                icon: '',
+                points:'100',
+            },
+            {
+                name: 'Meditate',
+                description: 'Meditate 30mn per day for a week',
+                icon: '',
+                points:'150',
+            },
+            {
+                name: 'Drink',
+                description: 'Drink 1L of water per day For 1 week',
+                icon: '',
+                points:'100',
+            },
+            {
+                name: 'Sleep',
+                description: 'Go to bed at 10 p.m. for 1 week.',
+                icon: '',
+                points:'150',
+            },
+        ]
     }
 ]
