7826853c66a6967f77e611b51fe429c7c7db87a9
Olyno <alexlew95@gmail.com>
2020-02-07 12:05:02 +0100

    Added challenges

---
+++ src/server/database/data/challengesCategory.ts
@@ -0,0 +1,44 @@
+export default [
+    {
+        name: 'Walk',
+        icon: '/icons/challenges/challenge1.svg',
+        color: '#FFCDE0',
+        challenges: [
+            {
+                name: 'Regular Walker',
+                description: 'Walk 20 minutes a day for 1 week',
+                icon: '/icons/challenges/challenge1.svg'
+            }
+        ]
+    },
+    {
+        name: 'To Start',
+        icon: '/icons/challenges/challenge2.svg',
+        color: '#D7FFCD',
+        challenges: []
+    },
+    {
+        name: 'Extremes sports',
+        icon: '/icons/challenges/challenge3.svg',
+        color: '#CDFFF7',
+        challenges: []
+    },
+    {
+        name: 'Normal sports',
+        icon: '/icons/challenges/challenge4.svg',
+        color: '#D79AFD',
+        challenges: []
+    },
+    {
+        name: 'Handisport',
+        icon: '/icons/challenges/challenge5.svg',
+        color: '#FAE2C2',
+        challenges: []
+    },
+    {
+        name: 'Healthy',
+        icon: '/icons/challenges/challenge6.svg',
+        color: '#F5F9AA',
+        challenges: []
+    }
+]
